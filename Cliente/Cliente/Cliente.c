5bdb8c9a7453a8e56dad9c862f21a6d34bff6fe2
dyaremyshyn <dmytroyaremyshyn@gmail.com>
2016-05-11 19:39:07 +0100

    cliente

--- Cliente/Cliente/Cliente.c
+++ Cliente/Cliente/Cliente.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 
 
-#define PIPE_NAME TEXT("\\\\.\\pipe\\envia")
+#define PIPE_N_WRITE TEXT("\\\\.\\pipe\\envia")
+#define PIPE_N_READ TEXT("\\\\.\\pipe\\recebe")
 
 int _tmain(int argc, LPTSTR argv[]) {
 	TCHAR buf[256];
@@ -13,33 +14,66 @@ int _tmain(int argc, LPTSTR argv[]) {
 	int i = 0;
 	BOOL ret;
 	DWORD n;
+	HANDLE hTread;
+	
 #ifdef UNICODE 
 	_setmode(_fileno(stdin), _O_WTEXT);
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
+	
 
-	_tprintf(TEXT("[LEITOR]Esperar pelo pipe '%s'(WaitNamedPipe)\n"), PIPE_NAME);
-	if (!WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
-		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (WaitNamedPipe)\n"), PIPE_NAME);
+	_tprintf(TEXT("[CLIENTE]Esperar pelo pipe '%s'(WaitNamedPipe)\n"), PIPE_N_WRITE);
+	if (!WaitNamedPipe(PIPE_N_WRITE, NMPWAIT_WAIT_FOREVER)) {
+		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (WaitNamedPipe)\n"), PIPE_N_WRITE);
 		exit(-1);
 	}
 
-	_tprintf(TEXT("[LEITOR] Ligação ao escritor... (CreateFile)\n"));
-	hPipe = CreateFile(PIPE_NAME, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	hTread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)RecebeDoServidor, NULL, 0, NULL);
+
+	_tprintf(TEXT("[CLIENTE] Ligação ao SERVIDOR... (CreateFile)\n"));
+	hPipe = CreateFile(PIPE_N_WRITE, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hPipe == NULL) {
-		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (CreateFile)\n"), PIPE_NAME);
+		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (CreateFile)\n"), PIPE_N_WRITE);
 		exit(-1);
 	}
-	_tprintf(TEXT("[LEITOR]Liguei-me...\n"));
+	_tprintf(TEXT("[CLIENTE]Liguei-me...\n"));
 	while (1) {
 		ret = ReadFile(hPipe, buf, sizeof(buf), &n, NULL);
 		buf[n / sizeof(TCHAR)] = '\0';
 		if (!ret || !n)
 			break;
-		_tprintf(TEXT("[LEITOR] Recebi %d bytes: '%s'... (ReadFile)\n"), n, buf);
+		_tprintf(TEXT("[CLIENTE] Recebi %d bytes: '%s'... (ReadFile)\n"), n, buf);
 	}
 	CloseHandle(hPipe);
 
 	Sleep(200);
 	return 0;
+}
+
+DWORD WINAPI RecebeDoServidor(LPVOID param) {
+	HANDLE hPipe;
+	TCHAR buf[256];
+	int i = 0;
+	BOOL ret;
+	DWORD n;
+
+	while (1) {
+		hPipe = CreateFile(PIPE_N_READ, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hPipe == NULL) {
+			_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (CreateFile)\n"), PIPE_N_READ);
+			exit(-1);
+		}
+		_tprintf(TEXT("[LEITOR]Liguei-me...\n"));
+		while (1) {
+			ret = ReadFile(hPipe, buf, sizeof(buf), &n, NULL);
+			buf[n / sizeof(TCHAR)] = '\0';
+			if (!ret || !n)
+				break;
+			_tprintf(TEXT("[LEITOR] Recebi %d bytes: '%s'... (ReadFile)\n"), n, buf);
+		}
+	
+	}
+	return 0;
+
+
 }
