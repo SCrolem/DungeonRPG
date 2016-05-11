51eeebc59f7ae5aef4b7e4783409ace22e2be03e
SCrolem <darkxield@gmail.com>
2016-05-11 18:50:23 +0100

    GGG

--- Cliente/Cliente/Cliente.c
+++ Cliente/Cliente/Cliente.c
@@ -5,7 +5,8 @@
 #include <stdio.h>
 
 
-#define PIPE_NAME TEXT("\\\\.\\pipe\\envia")
+#define PIPE_NAME1 TEXT("\\\\.\\pipe\\envia")
+#define PIPE_NAME2 TEXT("\\\\.\\pipe\\recebe")
 
 int _tmain(int argc, LPTSTR argv[]) {
 	TCHAR buf[256];
@@ -18,20 +19,27 @@ int _tmain(int argc, LPTSTR argv[]) {
 	_setmode(_fileno(stdout), _O_WTEXT);
 #endif
 
-	_tprintf(TEXT("[LEITOR]Esperar pelo pipe '%s'(WaitNamedPipe)\n"), PIPE_NAME);
-	if (!WaitNamedPipe(PIPE_NAME, NMPWAIT_WAIT_FOREVER)) {
-		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (WaitNamedPipe)\n"), PIPE_NAME);
+	_tprintf(TEXT("[LEITOR]Esperar pelo pipe '%s'(WaitNamedPipe)\n"), PIPE_NAME1);
+	
+	if (!WaitNamedPipe(PIPE_NAME1, NMPWAIT_WAIT_FOREVER)) {
+		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (WaitNamedPipe)\n"), PIPE_NAME1);
 		exit(-1);
 	}
 
 	_tprintf(TEXT("[LEITOR] Ligação ao escritor... (CreateFile)\n"));
-	hPipe = CreateFile(PIPE_NAME, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	hPipe = CreateFile(PIPE_NAME1, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 	if (hPipe == NULL) {
-		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (CreateFile)\n"), PIPE_NAME);
+		_tprintf(TEXT("[ERRO] Ligar ao pipe '%s'... (CreateFile)\n"), PIPE_NAME1);
 		exit(-1);
 	}
 	_tprintf(TEXT("[LEITOR]Liguei-me...\n"));
-	while (1) {
+	while (1) 
+	{   
+		_tprintf(TEXT("[ESCRITOR] Frase: "));
+		_fgetts(buf, 256, stdin);
+
+
 		ret = ReadFile(hPipe, buf, sizeof(buf), &n, NULL);
 		buf[n / sizeof(TCHAR)] = '\0';
 		if (!ret || !n)
